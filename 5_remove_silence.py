cf297d8d7f0a54254adce45a9c24f6dab5e3b667
Changhao Wu <changhaowu00@gmail.com>
2024-12-03 21:11:26 +0100

    refactor

--- 5_remove_silence.py
+++ 5_remove_silence.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-
+from utils import remove_silence
 
 # Example usage:
 input_audio = "/home/steven/Desktop/GIT/video-auto-dub/speech.acc"
