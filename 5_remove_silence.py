60d62273ecd424b67d59b105567e10725bea2501
Changhao Wu <changhaowu00@gmail.com>
2024-12-04 22:32:12 +0100

    Bug fix

--- 5_remove_silence.py
+++ 5_remove_silence.py
@@ -9,5 +9,3 @@ input_audio = "/home/steven/Desktop/GIT/video-auto-dub/speech.acc"
 output_audio = "removed_silence_audio.mp3"
 remove_silence(input_audio, output_audio)
 
-print(get_video_duration(INPUT_VIDEO_PATH))
-
