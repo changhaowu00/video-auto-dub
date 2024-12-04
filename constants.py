8a09c191320bfce25ac49985c857d55d70c0ae14
Changhao Wu <changhaowu00@gmail.com>
2024-12-04 22:32:46 +0100

    refactor

--- constants.py
+++
@@ -1,6 +0,0 @@
-# File config
-import os
-
-FILENAME = "venom1"
-INPUT_VIDEO_PATH = os.getcwd() + "/videos/" + FILENAME + ".mp4"
-OUTPUT_TEXT_PATH = os.getcwd() + "/output_files/" + FILENAME + ".txt"
